56308b580abadfb417bf2efdb7e904147b633b2d
edizaldogan <edizaldogan@gmail.com>
2026-07-15 12:06:10 +0200

    Spectrogram funciton added, global figure parameter 'white' is set

--- ASP_cell_phone.py
+++ ASP_cell_phone.py
@@ -59,3 +59,38 @@ is approximately centered on sample 3500.
 '''
 # %%
 
+'''
+As such, however, the speech waveform is not "readable", even by an
+expert phonetician. Its information (phonetic) content is hidden. In
+order to reveal it to the eyes, let us plot a spectrogram of the signal.
+For a better graphical result, we choose a wideband spectrogram, by
+imposing the length of each frame to be approximately 5 ms long (40
+samples) and a hamming weighting window.
+'''
+
+from scipy import signal
+
+def plot_spectrogram(audio, fs=8000):
+    """
+    Replicates MATLAB's specgram(speech, 512, 8000, hamming(40)) function.
+    Uses a 40-sample Hamming window for formant visualization.
+    """
+    f, t, Sxx = signal.spectrogram(audio, 
+                                   fs=fs,
+                                   window=np.hamming(40),
+                                   nperseg=40,
+                                   noverlap=20,
+                                   nfft=512)
+    plt.figure(figsize=(10, 8))
+    # Python uses 'viridis' color map as default. 
+    # Instead we will use MATLAB's 'jet' colormap for exact visual match
+    plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='auto', cmap='jet')
+    plt.title("Spectrogram")
+    plt.ylabel('Frequency (Hz)')
+    plt.xlabel('Time (s)')
+    plt.show()
+
+plot_spectrogram(audio)
+
+# %%
+
